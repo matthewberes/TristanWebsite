4c41251bffbb7b5635b1aff9573fff5109005685
Matt Beres <mattyberes@gmail.com>
2026-04-22 10:50:37 -0400

    rough gallery, services clean up

--- angular/src/app/gallery/gallery.component.ts
+++ angular/src/app/gallery/gallery.component.ts
@@ -1,11 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+  galleryImages = [
+    { src: 'images/gallery/image1.jpg', alt: 'Image 1' },
+    { src: 'images/gallery/image2.jpg', alt: 'Image 2' },
+    { src: 'images/gallery/image3.jpg', alt: 'Image 3' },
+    { src: 'images/gallery/image4.jpg', alt: 'Image 4' },
+  ];
 
 }
--- angular/src/app/services/services.component.ts
+++ angular/src/app/services/services.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, ViewportScroller } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,19 +8,31 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, AfterViewInit {
   constructor(private route: ActivatedRoute, private scroller: ViewportScroller) { }
 
   services = [
-    { title: 'Sedan/Coupe', description: 'Starting at $100', image: 'images/car.jpg', link: 'car' },
-    { title: 'Hatchback/CUV', description: 'Starting at $125', image: 'images/hatchback.jpg', link: 'cuv' },
-    { title: 'SUV/Truck', description: 'Starting at $150', image: 'images/suv.jpg', link: 'suv' },
+    { title: 'Sedan/Coupe', description: 'Professional car detailing for sedans and coupes', price: 'Starting at $100', image: 'images/car.jpg', link: 'car' },
+    { title: 'Hatchback/CUV', description: 'Comprehensive detailing for hatchbacks and CUVs', price: 'Starting at $125', image: 'images/hatchback.jpg', link: 'cuv' },
+    { title: 'SUV/Truck', description: 'Specialized care for SUVs and trucks', price: 'Starting at $150', image: 'images/suv.jpg', link: 'suv' },
   ];
 
+  private serviceToScroll: string | null = null;
+
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      const service = params['service'];
-      this.scroller.scrollToAnchor(service);
+    this.route.paramMap.subscribe(params => {
+      const pathService = params.get('service');
+      if (pathService) {
+        this.serviceToScroll = pathService;
+      }
     });
+  }
+
+  ngAfterViewInit(): void {
+    if (this.serviceToScroll) {
+      setTimeout(() => {
+        this.scroller.scrollToAnchor(this.serviceToScroll!);
+      }, 0);
+    }
   }
 }
